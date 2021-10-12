24296d90ab325eb57961a1f3b9fea9fbcbc7ff70
SouryaAgrawal2998 <81215092+SouryaAgrawal2998@users.noreply.github.com>
2021-10-12 12:00:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-var Name_Of_The_Guest_Array = [];
-function Submit() {
-   var Guest_name = document.getElementById("Enter_name").value;
-   Name_Of_The_Guest_Array.push(Guest_name);
-   document.getElementById("Display_names").innerHTML = Name_Of_The_Guest_Array;
-   console.log(Name_Of_The_Guest_Array);
-   var length_of_the_array = Name_Of_The_Guest_Array.length;
-    console.log(length_of_the_array);
-}
-function Show_list() {
-    var i = Name_Of_The_Guest_Array.join("<br>");
-    console.log(Name_Of_The_Guest_Array);
-    document.getElementById("Show_names").innerHTML = i.toString();
-}
-function sorting() {
-    Name_Of_The_Guest_Array.sort();
-    var i = Name_Of_The_Guest_Array.join("<br>");
-    console.log(Name_Of_The_Guest_Array);
-    document.getElementById("Sorted_names").innerHTML = i.toString();
-}
-function Search() {
-    var s = document.getElementById("Search_name").value;
-    var found = 0;
-    var j;
-    for(j=0; j<Name_Of_The_Guest_Array.length; j++) {
-        if(s==Name_Of_The_Guest_Array[j]) {
-            found = found+1;
-        }
+menu_list_array = ["Veggie Pizza","Cheese Pizza","Pepperoni Pizza", "Margherita Pizza"];
+    
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
     }
-    document.getElementById("Search_names").innerHTML = "Name Found "+found+" Time/s";
-    console.log("Found Name "+found+" Time/s");
+     htmldata=htmldata+"</ol>"
+     document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+
+        htmldata=htmldata+'<div class="card">' +'<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+    }
+      htmldata=htmldata+"</section>"
+      document.getElementById("display_addedmenu").innerHTML = htmldata;
+      document.getElementById("add_item").value = "";
 }
